1a5a199163512069bb9c10ca6d3f51df7d7d4e84
Mihai Pirvulet <mitapirvuet@gmail.com>
2020-09-05 20:17:06 +0300

    add jquery materials

---
+++ jquery/skel/main.js
@@ -0,0 +1,22 @@
+const cities = {
+    "UK" : [
+        {
+            name: "Birmingham",
+            id: 1
+        },
+        {
+            name: "London",
+            id: 2
+        }
+    ],
+    "RO" : [
+        {
+            name: "Bucuresti",
+            id: 3
+        },
+        {
+            name: "Cluj-Napoca",
+            id: 4
+        }
+    ]
+};
